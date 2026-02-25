3da0d9e0e4689b50df78a870e087e41971aa8921
JOSnow985 <jaden.olvera@students.snow.edu>
2026-02-24 19:28:17 -0700

    Environment set up, builds

---
+++ Lab06/DNode.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab06;
+
+public class DNode<T> 
+{
+    public T Value { get; set; }
+    public DNode<T>? Previous { get; set; }
+    public DNode<T>? Next { get; set; }
+}
---
+++ Lab06/DoublyLinkedList.cs
@@ -0,0 +1,40 @@
+﻿namespace Lab06;
+
+public class DoublyLinkedList<T> : IDoubleEndedCollection<T> 
+{
+    private DNode<T>? _head = null;
+    private DNode<T>? _tail = null;
+    public int Length { get; } = 0;
+    public T First { get; }
+    public T Last { get; }
+
+    // TODO: implement the methods of the interface
+    public void AddLast(T value)
+    {
+        
+    }
+    public void AddFirst(T value)
+    {
+        
+    }
+    public void RemoveFirst()
+    {
+        
+    }     
+    public void RemoveLast()
+    {
+        
+    }                
+    public void InsertAfter(DNode<T> node, T value)
+    {
+        
+    }
+    public void RemoveByValue(T value)
+    {
+
+    }
+    public void ReverseList()
+    {
+        
+    }
+}
